dff96d991e6cfd10c6f956c5b2d18d51fc1ad037
suyash898 <114183761+suyash898@users.noreply.github.com>
2022-09-24 14:41:34 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.math.*;
+class Student
+{
+private int rollNumber;
+private String firstName;
+private String lastName;
+private Date dateOfBirth;
+private BigDecimal feePaid;
+private boolean isIndian;
+}
